7f8eb41270688c917f8252d2250e26b2d945f457
robester0403 <robkso@gmail.com>
2022-09-17 16:57:45 -0400

    Added Tag Entity, DataSource instead of deprecated CreateConnection. Many to Many relationships

---
+++ src/entities/Tag.ts
@@ -0,0 +1,27 @@
+import { Field, Int, ObjectType } from "type-graphql";
+import {
+  BaseEntity,
+  Column,
+  Entity,
+  Index,
+  ManyToMany,
+  PrimaryGeneratedColumn,
+} from "typeorm";
+import { Task } from "./Task";
+
+@Entity("tag")
+@ObjectType()
+export class Tag extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  @Field(() => Int)
+  id: number;
+
+  @Index({ unique: true })
+  @Column("varchar", { length: 100 })
+  @Field(() => String, { nullable: false })
+  tag_name: string;
+
+  @ManyToMany(() => Task, { nullable: true })
+  @Field(() => [Task])
+  tasks: Task[] | null;
+}
